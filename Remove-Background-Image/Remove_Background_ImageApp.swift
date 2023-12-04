9daf62543a53bd433ce0b226b2023bd9c692f7a2
mculha <melih.culha.09@gmail.com>
2023-12-04 23:04:54 +0300

    Initial Commit

---
+++ Remove-Background-Image/Remove_Background_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Remove_Background_ImageApp.swift
+//  Remove-Background-Image
+//
+//  Created by Melih Çulha on 4.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Remove_Background_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
